d60f978d5ea0f4d949942b5386ce543b54d380bc
ReflextionsDev <ReflextionsDev@gmail.com>
2021-12-13 16:12:33 -0500

    Start building node interface

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
+// Eventually allow file upload, even if it's temp / not saved
+
 $(function () {
 
 
-    $(".draggable").draggable({ containment: "#containment-wrapper", scroll: false });
+  $(".draggable").draggable({ containment: "#drag-box", scroll: false });
+
 
 });
+
+
+
+
+
+$('#trigger').click(function () {
+  $('#overlay').fadeIn(300);
+});
+$('#close').click(function () {
+  $('#overlay').fadeOut(300);
+});
+
+// Popups
+// - Settings
+// - Assets (upload, do saving later)
+// - Node
