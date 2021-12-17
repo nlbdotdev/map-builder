5d23aec9fea0d5a3e3693363cfa27d0b2e88bf51
ReflextionsDev <ReflextionsDev@gmail.com>
2021-12-17 13:03:50 -0500

    Add mode toggle

--- main.js
+++ main.js
@@ -1,12 +1,100 @@
+// --- MODES ---
+
+$('#modes-play').click(() => {
+  changeMode('play')
+})
+
+$('#modes-edit').click(() => {
+  changeMode('edit')
+})
+
+function changeMode(mode){
+  if (mode === 'play') {
+    $('#editor').hide()
+    $('#map').show()
+  } else if (mode === 'edit') {
+    $('#editor').show()
+    $('#map').hide()
+  }
+}
+
+// --- EDITOR ---
+
+// live update icon image
+// create / update object on SAVE
+// make play / edit toggle work
+// doodle css for editor?
+// https://www.reddit.com/r/Frontend/comments/rgz6d7/doodle_css_a_simple_hand_drawn_htmlcss_theme/
+
+function MapNode(id, icon, bg, music, url, name, lore) {
+    this.id = id
+    this.icon = icon
+    this.bg = bg
+    this.music = music
+    this.url = url
+    this.name = name
+    this.lore = lore
+  }
+
+const MapNodes = []
+
+$('.block-save').click((e) => {
+
+  console.log(e.target)
+
+  const id = e.target.classList[1].split("-")[1]
+  const icon = $(`.block-${id} > .block-icon > .block-icon-select`)[0].value
+  const bg = ''
+  const music = ''
+  const url = ''
+  const name = $(`.block-${id} > .block-name > .block-name-input`)[0].value
+  const lore = ''
+  const node = new MapNode(id, icon, bg, music, url, name, lore)
+
+
+  // current dupes
+  MapNodes.push(node)
+})
+
+// push x to nodes array...
+
+
+
+
+$('.block-icon-select').on('change', (e) => {
+  // alert('Value change to ' + e.target.value)
+  changeBlockIcon(1, e.target.value)
+})
+
+
+
+function changeBlockIcon(id, image) {
+  // Select proper block
+  let img = $(`.block-${id} > .block-icon > .block-icon-thumbnail`)[0]
+  img.src = `./images/icons/${image}.png`;
+
+  console.log(image)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// --- MAP ---
+
 // Eventually allow file upload, even if it's temp / not saved
 
 $(() => {
   $(".draggable").draggable({ containment: "#drag-box", scroll: false });
 });
-
-
-
-
 
 // $('#trigger').click((e) => {
 //   console.log(e.target)
@@ -51,7 +139,7 @@ function openMenu(type, subtype) {
 
 }
 
-function closeMenu(){
+function closeMenu() {
 
 
 
