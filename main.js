a68a41dda0bd31103464a2f5b19209806838ac91
ReflextionsDev <ReflextionsDev@gmail.com>
2021-12-18 22:44:50 -0500

    Add block button

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 // --- MODES ---
 
-
 // load in nodes
 $('#modes-play').click(() => {
   changeMode('play')
   updateNodes()
 })
-
 
 function updateNodes() {
   // for each mapNodes array, select node-id
@@ -17,19 +15,16 @@ function updateNodes() {
 
     console.log(node.id)
 
-     $(`#node-${id} > .title`)[0].innerHTML = node.name
+    $(`#node-${id} > .title`)[0].innerHTML = node.name
 
     console.log(node.icon)
 
-     $(`#node-${id} > .icon > img`)[0].src = node.icon
-
-
+    $(`#node-${id} > .icon > img`)[0].src = node.icon
 
     console.log(node.name)
 
   }
 }
-
 
 
 $('#modes-edit').click(() => {
@@ -45,6 +40,8 @@ function changeMode(mode) {
     $('#map').hide()
   }
 }
+
+
 
 // --- EDITOR ---
 
@@ -68,6 +65,8 @@ function MapNode(id, icon, bg, music, url, name, lore) {
 
 const MapNodes = []
 
+// make it dynamically build ur stuff
+
 $('.block-save').click((e) => {
 
   console.log(e.target)
@@ -89,6 +88,7 @@ $('.block-save').click((e) => {
 // push x to nodes array...
 
 
+////
 
 
 $('.block-icon-select').on('change', (e) => {
@@ -104,10 +104,7 @@ function changeBlockIcon(id, image) {
   img.src = `./images/icons/${image}.png`;
 
   console.log(image)
-
 }
-
-
 
 
 function getIconPath(icon) {
@@ -116,8 +113,83 @@ function getIconPath(icon) {
 
 
 
+//
+
+let blocks = 1
+
+$('.block-add').click(() => {
+  addBlock()
+})
+
+function addBlock() {
+  blocks++
+
+  const currentBlock = blocks
+
+  const newBlock = $(`
+  <div class="block block-${blocks}">
+  <div class="block-icon">
+      <img src="./images/icons/trees.png" alt="" class="block-icon-thumbnail">
+  </div>
+
+  <div>
+      <label for="icon" class="block-text">Icon: </label>
+      <select name="icon" class="block-icon-select">
+          <option value="trees">Trees</option>
+          <option value="lake">Lake</option>
+          <option value="mountain">Mountain</option>
+          <option value="village">Village</option>
+      </select>
+      <div class="block-name">
+          <label for="name" class="block-text">Name: </label>
+          <input type="text" class="block-name-input" value="Jeff">
+      </div>
+      <div class="block-lore">
+          <label for="name" class="block-lore">Lore: </label>
+          <textarea class="block-lore"></textarea>
+      </div>
+  </div>
+
+  <div>
+      <div class="block-BG">
+          <label for="BG" class="block-text">BG URL: </label>
+          <input type="text class=" block-BG" value="./images/backgrounds/2.jpg">
+      </div>
+      <div class="block-music">
+          <label for="music" class="block-text">Music URL: </label>
+          <input type="text class=" block-music" value="./audio/music/tavern.mp3">
+      </div>
+      <div class="block-ambiance">
+          <label for="ambiance" class="block-text">Ambiance URL: </label>
+          <input type="text class=" block-ambiance" value="./audio/music/plains.mp3">
+      </div>
+  </div>
 
 
+  <div>
+      <div>
+          <button class="block-save block-${blocks}">SAVE</button>
+      </div>
+      <br>
+      <div class="block-delete">
+          <button>DELETE</button>
+      </div>
+  </div>
+  `)
+
+
+  $('#blocks').append(newBlock)
+
+
+  $(`.block-${currentBlock} .block-icon-select`).on('change', (e) => {
+    // alert('Value change to ' + e.target.value)
+    changeBlockIcon(currentBlock, e.target.value)
+  })
+
+  
+
+
+}
 
 
 
